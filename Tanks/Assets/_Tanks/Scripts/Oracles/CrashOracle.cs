c6b18888080c89b402f60ae55b4f481e6fc6b1db
luketto <115785443+luckybros@users.noreply.github.com>
2026-06-17 12:02:54 +0200

    added data for executable

--- Tanks/Assets/_Tanks/Scripts/Oracles/CrashOracle.cs
+++ Tanks/Assets/_Tanks/Scripts/Oracles/CrashOracle.cs
@@ -49,43 +49,5 @@ namespace Tanks.Complete
                 }
             }
         }
-        
-        /*
-        void Update()
-        {
-            timer += Time.deltaTime;
-
-            if (timer >= interval)
-            {
-                counter++;
-                sideChannel.SendStringToPython($"ALIVE {counter}");
-                timer = 0f;
-            }
-
-            if (Input.GetKeyDown(KeyCode.K))
-            {
-                Debug.Log("--- Crash without logging");
-                System.Diagnostics.Process.GetCurrentProcess().Kill();
-            }
-
-            if (Input.GetKeyDown(KeyCode.H))
-            {
-                Debug.Log("Hang infinite loop");
-                while (true) {}
-            }
-
-            if (Input.GetKeyDown(KeyCode.C))
-            {
-                // Debug.LogError("--- ATTIVAZIONE CRASH: Forzatura eccezione");
-                Debug.LogError("Simulated crash for testing");
-                Invoke("KillProcess", 2f);
-            }
-        }
-
-        void KillProcess()
-        {
-            System.Diagnostics.Process.GetCurrentProcess().Kill();
-        }
-        */
     }
 }
